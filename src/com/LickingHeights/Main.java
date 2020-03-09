428beaa3c2d3d3db8a6e5f014f54b019acf60c53
Janae Anderson <janae.anderson.2022@student.lhschools.org>
2020-03-09 13:07:45 -0400

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,36 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-	// write your code here
+        testOddness();
+
+
+    }
+
+
+
+    public static void testOddness() {
+
+        int pNumber = 12;
+
+        System.out.println("testOddness");
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Type a positive number");
+        pNumber = keyboard.nextInt();
+
+
+        System.out.println("Number is odd "+ (pNumber % 2 !=0));
+
+
+
+
+
     }
 }
